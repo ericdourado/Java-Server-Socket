307ec8cf4a19c705f49487d813d27f89390e15d4
ericdourado <ericdourado1@hotmail.com>
2022-04-18 22:42:05 -0300

    Criado o segundo cliente e sistema de chat funcionando

--- JavaChatVersion 0.1.2/chat-blocking/src/main/java/com/mycompany/chat/blocking/ChatClient2.java
+++ JavaChatVersion 0.1.2/chat-blocking/src/main/java/com/mycompany/chat/blocking/ChatClient2.java
@@ -17,20 +17,31 @@ import java.util.Scanner;
 public class ChatClient2 {
     private static void MensagemLoop() throws IOException{
         Socket socket = new Socket("localhost", 5555);
-        String msg;
+        String msg = null;
+        String msgreceptora = null;
+        String aux = "";
         Scanner scanner = new Scanner(System.in);
         //Cria objetos de streams de inputs e outputs
         ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
         ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+        System.out.print("Digite uma mensagem (ou 'sair' para finalizar):\n");
+        System.out.println("Aperte enter para ativar o Chat");
         do{
-           System.out.print("Digite uma mensagem (ou 'sair' para finalizar):"); 
-           msg = scanner.nextLine();
-           //envia mensagem pro servidor
-           output.writeUTF(msg);
-           output.flush();
-           //Recebe mensagem do servidor na mesma variável
-           msg = input.readUTF();
-           System.out.println("Mensagem do servidor: " + socket.getRemoteSocketAddress() + "  :" + msg);
+            
+            
+            if (aux == ""){
+            System.out.println("CLIENTE JOAO: ");
+            msg = scanner.next();
+            
+            //envia mensagem pro servidor
+            output.writeUTF(msg);
+            output.flush();
+            }
+            
+            //Recebe msg do servidor
+            msgreceptora = input.readUTF();
+            System.out.println("Mensagem do Eric: "  + msgreceptora);
+           
         }while(!msg.equalsIgnoreCase("sair"));
         
     }
--- JavaChatVersion 0.1.2/chat-blocking/src/main/java/com/mycompany/chat/blocking/ChatServer.java
+++ JavaChatVersion 0.1.2/chat-blocking/src/main/java/com/mycompany/chat/blocking/ChatServer.java
@@ -25,23 +25,48 @@ public class ChatServer {
         Socket socket = serverSocket.accept();
         return socket;
     }
+    private Socket esperaConexao2() throws IOException {
+        Socket socket = serverSocket.accept();
+        return socket;
+    }
 
-    private void trataConexao(Socket socket) throws IOException {
+    private void trataConexao(Socket socket, Socket socket2) throws IOException {
         try {
-            
-            
-            
+
             //OBJETOS DE ENTRADA E SAÍDA
             
             ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
             ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+            
+            ObjectOutputStream output2 = new ObjectOutputStream(socket2.getOutputStream());
+            ObjectInputStream input2 = new ObjectInputStream(socket2.getInputStream());
+            String msg;
+            String msg2 = null;
             while(true){
-            //RECEBE MSG
-            String msg = input.readUTF();
+            
+            //MENSAGENS DO CLIENTE 1
+            //RECEBE MSG 
+            msg = input.readUTF();
             System.out.println("Mensagem do Cliente: " + socket.getRemoteSocketAddress() + "  :" + msg);
+                        
             //ENVIA MSG
-            output.writeUTF("Hello world");
+            output2.writeUTF(msg);
+            output2.flush();
+             
+            //MENSAGENS DO CLIENTE 2
+            ////RECEBE MSG 
+            msg2 = input2.readUTF();
+            System.out.println("Mensagem do Cliente: " + socket2.getRemoteSocketAddress() + "  :" + msg2);
+
+            //ENVIA MSG 
+            output.writeUTF(msg2);
             output.flush();
+            
+            
+            
+           
+            
+           
             //fechar input e output antes de fechar servidor
             }
 
@@ -59,10 +84,16 @@ public class ChatServer {
         try {
             ChatServer server = new ChatServer();
             server.criarServerSocket(5555);
-            System.out.println("..................Aguardando Conexão...............");
+            System.out.println("..................Aguardando Conexão Cliente 1...............");
             Socket socket = server.esperaConexao();
+            System.out.println("Cliente 1 Conectado...");
+            System.out.println("..................Aguardando Conexão Cliente 2...............");
+            Socket socket2 = server.esperaConexao2();
+            System.out.println("Cliente 2 conectado....");
+            System.out.println("Os dois cliente foram conectados...");
             System.out.println("Cliente " + socket.getRemoteSocketAddress() + " Conectado");
-            server.trataConexao(socket);
+            System.out.println("Cliente " + socket2.getRemoteSocketAddress() + " Conectado");
+            server.trataConexao(socket, socket2);
             
 
         } catch (IOException ex) {
